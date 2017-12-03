5531e8bdf025502bb01ee894a30008792a2c6b86
arpruss <arpruss@gmail.com>
2017-12-03 15:15:52 -0600

    weak pma

--- clone.sh
+++ clone.sh
@@ -1,6 +1,6 @@
 cp LICENSE *.c *.cpp usb_hid.h usb_hid_device.h ../KeyboardMouse
 cp LICENSE *.c *.cpp usb_hid.h usb_hid_device.h ../Joystick
-cp usb_setup.cpp ../USBMIDI
+cp pma.c ../USBMIDI
 sed s/KeyboardMouse/KeyboardMouseJoystick/ < library.properties > ../KeyboardMouse/library.properties
 sed s/KeyboardMouse/Joystick/ < library.properties > ../Joystick/library.properties
 sed s/KeyboardMouse/KeyboardMouseJoystick/ < README.md > ../KeyboardMouse/README.md

18bdabfa1c2d277e138795b5c948781c45190ead
arpruss <arpruss@gmail.com>
2017-12-03 22:36:56 -0600

    fix descriptions

--- clone.sh
+++ clone.sh
@@ -1,7 +1,7 @@
 cp LICENSE *.c *.cpp dummyserial.h usb_hid.h usb_hid_device.h ../KeyboardMouse
 cp LICENSE *.c *.cpp dummyserial.h usb_hid.h usb_hid_device.h ../Joystick
 cp dummyserial.h pma.c ../USBMIDI
-sed s/KeyboardMouse/KeyboardMouseJoystick/ < library.properties > ../KeyboardMouse/library.properties
-sed s/KeyboardMouse/Joystick/ < library.properties > ../Joystick/library.properties
-sed s/KeyboardMouse/KeyboardMouseJoystick/ < README.md > ../KeyboardMouse/README.md
-sed s/KeyboardMouse/Joystick/ < README.md > ../Joystick/README.md
+sed s/KeyboardMouseJoystick/KeyboardMouse/ < library.properties > ../KeyboardMouse/library.properties
+sed s/KeyboardMouseJoystick/Joystick/ < library.properties > ../Joystick/library.properties
+sed s/KeyboardMouseJoystick/KeyboardMouse/ < README.md > ../KeyboardMouse/README.md
+sed s/KeyboardMouseJoystick/Joystick/ < README.md > ../Joystick/README.md
